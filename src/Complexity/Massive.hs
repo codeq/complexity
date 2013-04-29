f857691689a636d07c6303f1858fa92d2aee817e
Mike Yumatov <mike@yumatov.org>
2013-04-30 00:26:56 +0400

    Use (++) instead of concat

--- src/Complexity/Massive.hs
+++ src/Complexity/Massive.hs
@@ -24,10 +24,10 @@ instance Massive a => Massive (Maybe a) where
   mass coef (Just a) = mass coef a
 
 concatMass2 :: (Massive a, Massive b) => Float -> a -> b -> [Mass]
-concatMass2 coef x1 x2 = concat [mass coef x1, mass coef x2]
+concatMass2 coef x1 x2 = mass coef x1 ++ mass coef x2
 
 concatMass3 :: (Massive a, Massive b, Massive c) => Float -> a -> b -> c -> [Mass]
-concatMass3 coef x1 x2 x3 = concat [mass coef x1, mass coef x2, mass coef x3]
+concatMass3 coef x1 x2 x3 = mass coef x1 ++ mass coef x2 ++ mass coef x3
 
 concatMass4 :: (Massive a, Massive b, Massive c, Massive d) => Float -> a -> b -> c -> d -> [Mass]
-concatMass4 coef x1 x2 x3 x4 = concat [mass coef x1, mass coef x2, mass coef x3, mass coef x4]
+concatMass4 coef x1 x2 x3 x4 = mass coef x1 ++ mass coef x2 ++ mass coef x3 ++ mass coef x4
